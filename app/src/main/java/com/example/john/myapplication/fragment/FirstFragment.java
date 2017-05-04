3f35617bf8fae0785a25f34eedf92219353a99c3
leizixing <1435606838@qq.com>
2017-05-04 16:29:06 +0800

    2017年5月4日15:55:33

--- app/src/main/java/com/example/john/myapplication/fragment/FirstFragment.java
+++ app/src/main/java/com/example/john/myapplication/fragment/FirstFragment.java
@@ -3,14 +3,11 @@ package com.example.john.myapplication.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.example.john.myapplication.activity.DetailActivity;
 import com.example.john.myapplication.activity.R;
@@ -38,7 +35,7 @@ public class FirstFragment extends Fragment implements MyItemClickListener{
         recyclerView.setLayoutManager(linearLayoutManager);
         FruitAdapter fruitAdapter = new FruitAdapter(fruits);
         recyclerView.setAdapter(fruitAdapter);
-        fruitAdapter.setOnItemClickListener(this); 
+        fruitAdapter.setOnItemClickListener(this);
         return view;
     }
 
